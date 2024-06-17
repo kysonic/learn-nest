214467a51aac9a5282b1fdd9b77f2295fa39f01b
Anton Miroshnochenko <soooyc@gmail.com>
2024-06-17 11:53:40 +0300

    chore: typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
-
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'spotikuser',
+      password: 'potikpassword',
+      database: 'learn-nest',
+      autoLoadEntities: true,
+      synchronize: true, // disable in prod
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
